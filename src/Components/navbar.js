c78e6e4669ed450aecfcad59901ab26f10941a06
Ashish Sengar <techie.sengar1924@gmail.com>
2022-12-01 11:17:28 +0530

    f

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -3,8 +3,17 @@ import React from 'react'
 export default function navbar() {
   return (
     <>
-      hello baby where are you
-      xsadfghm,.,mnbcvxzdsbnm,mnbvcx
+      <nav id='main'>
+
+      <div id='logo'>
+            <h2>12-B </h2>
+      </div>
+
+      <div id='detail'>
+
+      </div>
+        
+      </nav>
     </>
   )
 }
