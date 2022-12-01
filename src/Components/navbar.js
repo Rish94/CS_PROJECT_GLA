c54a7fad188ae3ecd59ce770b037ee819d70efbd
RISHABH AGARWAL <73248554+Rish94@users.noreply.github.com>
2022-12-01 11:00:14 +0530

    c

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -3,14 +3,7 @@ import React from 'react'
 export default function navbar() {
   return (
     <>
-    mxsabhd 
     
-    moj kar di bete
-    hbvhfhv
-    wdcefrgthryjtu
-
-    okk baby
-    xcfsvgbhnj
     </>
   )
 }
